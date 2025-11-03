7abcc0b0b2c4608244a3f7836df33d1ade1d96bd
Jakub Drozd <jdrozd2003@gmail.com>
2025-11-03 15:00:31 +0100

    login and register page can now send form data to backend in JSON

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -1,82 +1,101 @@
 <script lang="ts">
+	const PUBLIC_API_URL = "http://localhost:5173"
 
-  let userLog = {
-    usernameEmail: '',
-    password: '',
-  };
+	let userLog = {
+		email: '',
+		password: ''
+	};
 
-  function handleSubmit() {
-   // TODO: fetch data to API
-    console.log('Dane do rejestracji:', userLog);
-    alert('Formularz wysłany! Sprawdź konsolę.');
-  }
+	async function handleSubmit() {
+		let error = null;
+
+		try {
+			const response = await fetch(`${PUBLIC_API_URL}/login`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(userLog)
+			});
+
+			if (response.ok) {
+				const tokens = await response.json();
+				console.log('Login udany, tokeny:', tokens);
+				// goto('/dashboard');
+			} else {
+				const errorData = await response.json();
+				error = errorData.detail || `Server error: ${response.status}`;
+			}
+		} catch (e) {
+			error = 'Couldnt connect to API.';
+			console.error(e);
+		}
+	}
 </script>
 
-<div class="min-h-screen bg-gray-100 flex flex-col lg:flex-row">
-  <div class="w-full lg:w-1/3 flex justify-center p-8 text-white
-              bg-linear-to-br from-teal-700 via-violet-800 to-teal-700">
-    <div class="absolute inset-0 z-0 backdrop-filter backdrop-blur-3xl"></div>
-    <div class="relative z-10 max-w-md lg:mt-20 text-left lg:text-left">
-      <h1 class="text-4xl lg:text-5xl font-extrabold leading-tight mb-4 opacity-90">
-        The bridge between ideas.
-      </h1>
-      <p class="text-2xl lg:text-3xl font-semibold opacity-80">
-        Brings people together.
-      </p>
-    </div>
-  </div>
+<div class="flex min-h-screen flex-col bg-gray-100 lg:flex-row">
+	<div
+		class="bg-linear-to-br flex w-full justify-center from-teal-700 via-violet-800
+              to-teal-700 p-8 text-white lg:w-1/3"
+	>
+		<div class="absolute inset-0 z-0 backdrop-blur-3xl backdrop-filter"></div>
+		<div class="relative z-10 max-w-md text-left lg:mt-20 lg:text-left">
+			<h1 class="mb-4 text-4xl font-extrabold leading-tight opacity-90 lg:text-5xl">
+				The bridge between ideas.
+			</h1>
+			<p class="text-2xl font-semibold opacity-80 lg:text-3xl">Brings people together.</p>
+		</div>
+	</div>
 
-  <div class="w-full lg:w-2/3 flex z-10 items-center justify-center p-8 bg-white lg:rounded-l-4xl">
-    <div class="max-w-2xl w-full mb-52">
-      <h2 class="text-5xl font-bold mb-20 text-gray-800 text-center lg:text-left">Log in</h2>
+	<div class="lg:rounded-l-4xl z-10 flex w-full items-center justify-center bg-white p-8 lg:w-2/3">
+		<div class="mb-52 w-full max-w-2xl">
+			<h2 class="mb-20 text-center text-5xl font-bold text-gray-800 lg:text-left">Log in</h2>
 
-      <form on:submit|preventDefault={handleSubmit} class="space-y-6">
-        <div>
-          <label for="username" class="block text-sm font-medium text-gray-700">Email or username</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            bind:value={userLog.usernameEmail}
-            class="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-lg shadow-sm
-                   focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            placeholder="your.email@example.com"
-            required
-          />
-        </div>
+			<form on:submit|preventDefault={handleSubmit} class="space-y-6">
+				<div>
+					<label for="username" class="block text-sm font-medium text-gray-700">Email</label>
+					<input
+						type="text"
+						id="username"
+						name="username"
+						bind:value={userLog.email}
+						class="mt-1 block w-full rounded-lg border border-gray-300 px-4 py-3 shadow-sm
+                   focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+						placeholder="your.email@example.com"
+						required
+					/>
+				</div>
 
-        <div>
-          <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            bind:value={userLog.password}
-            class="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-lg shadow-sm
-                   focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            placeholder="Minimum 8 characters"
-            required
-          />
-        </div>
+				<div>
+					<label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+					<input
+						type="password"
+						id="password"
+						name="password"
+						bind:value={userLog.password}
+						class="mt-1 block w-full rounded-lg border border-gray-300 px-4 py-3 shadow-sm
+                   focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+						placeholder="Minimum 8 characters"
+						required
+					/>
+				</div>
 
-        <div>
-          <button
-            type="submit"
-            class="w-full flex justify-center py-3 px-4 border border-transparent rounded-lg
-                   shadow-sm text-lg font-medium text-white bg-teal-950 hover:bg-cyan-800
-                   focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Log in
-          </button>
-        </div>
-      </form>
+				<div>
+					<button
+						type="submit"
+						class="flex w-full justify-center rounded-lg border border-transparent bg-teal-950 px-4
+                   py-3 text-lg font-medium text-white shadow-sm hover:bg-cyan-800
+                   focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+					>
+						Log in
+					</button>
+				</div>
+			</form>
 
-      <p class="mt-8 text-left text-sm text-gray-600">
-        Don't have an account yet?
-        <a href="/register" class="font-medium text-indigo-600 hover:text-indigo-500">
-          Sign up
-        </a>
-      </p>
-    </div>
-  </div>
+			<p class="mt-8 text-left text-sm text-gray-600">
+				Don't have an account yet?
+				<a href="/register" class="font-medium text-indigo-600 hover:text-indigo-500"> Sign up </a>
+			</p>
+		</div>
+	</div>
 </div>
--- frontend/src/routes/register/+page.svelte
+++ frontend/src/routes/register/+page.svelte
@@ -1,130 +1,151 @@
 <script lang="ts">
-  import type { UserCreate } from '$lib/models/user.models';
+	import type { UserCreate, UserGet } from '$lib/models/user.models';
+	const PUBLIC_API_URL = "http://localhost:5173"
 
-  let newUser: UserCreate = {
-    username: '',
-    name: '',
-    surname: '',
-    email: '',
-    password: '',
-    is_admin: false
-  };
+	let newUser: UserCreate = {
+		username: '',
+		name: '',
+		surname: '',
+		email: '',
+		password: '',
+		is_admin: false
+	};
 
-  function handleSubmit() {
-   // TODO: fetch data to API
-    console.log('Dane do rejestracji:', newUser);
-    alert('Formularz wysłany! Sprawdź konsolę.');
-  }
+	async function handleSubmit() {
+		let error = null;
+
+		try {
+			const response = await fetch(`${PUBLIC_API_URL}/register`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(newUser)
+			});
+
+			if (response.ok) {
+				const registeredUser: UserGet = await response.json();
+				// console.log('Zarejestrowano użytkownika:', registeredUser);
+				// goto('/login');
+			} else {
+				const errorData = await response.json();
+				error = errorData.detail || `Server error: ${response.status}`;
+			}
+		} catch (e) {
+			error = 'Couldnt connect to API.';
+			console.error(e);
+		}
+	}
 </script>
 
-<div class="min-h-screen bg-gray-100 flex flex-col lg:flex-row">
-  <div class="w-full lg:w-1/3 flex justify-center p-8 text-white
-              bg-linear-to-br from-teal-700 via-violet-800 to-teal-700">
-    <div class="absolute inset-0 z-0 backdrop-filter backdrop-blur-3xl"></div>
-    <div class="relative z-10 max-w-md lg:mt-20 text-left lg:text-left">
-      <h1 class="text-4xl lg:text-5xl font-extrabold leading-tight mb-4 opacity-90">
-        The bridge between ideas.
-      </h1>
-      <p class="text-2xl lg:text-3xl font-semibold opacity-80">
-        Brings people together.
-      </p>
-    </div>
-  </div>
+<div class="flex min-h-screen flex-col bg-gray-100 lg:flex-row">
+	<div
+		class="bg-linear-to-br flex w-full justify-center from-teal-700 via-violet-800
+              to-teal-700 p-8 text-white lg:w-1/3"
+	>
+		<div class="absolute inset-0 z-0 backdrop-blur-3xl backdrop-filter"></div>
+		<div class="relative z-10 max-w-md text-left lg:mt-20 lg:text-left">
+			<h1 class="mb-4 text-4xl font-extrabold leading-tight opacity-90 lg:text-5xl">
+				The bridge between ideas.
+			</h1>
+			<p class="text-2xl font-semibold opacity-80 lg:text-3xl">Brings people together.</p>
+		</div>
+	</div>
 
-  <div class="w-full lg:w-2/3 flex z-10 items-center justify-center p-8 bg-white lg:rounded-l-4xl">
-    <div class="max-w-2xl w-full mb-40">
-      <h2 class="text-5xl font-bold mb-20 text-gray-800 text-center lg:text-left">Create an Account</h2>
+	<div class="lg:rounded-l-4xl z-10 flex w-full items-center justify-center bg-white p-8 lg:w-2/3">
+		<div class="mb-40 w-full max-w-2xl">
+			<h2 class="mb-20 text-center text-5xl font-bold text-gray-800 lg:text-left">
+				Create an Account
+			</h2>
 
-      <form on:submit|preventDefault={handleSubmit} class="space-y-6">
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <div>
-            <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              bind:value={newUser.name}
-              class="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-lg shadow-sm
-                     focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              placeholder="Your name"
-              required
-            />
-          </div>
-          <div>
-            <label for="surename" class="block text-sm font-medium text-gray-700">Last name</label>
-            <input
-              type="text"
-              id="surename"
-              name="surename"
-              bind:value={newUser.surname}
-              class="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-lg shadow-sm
-                     focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              placeholder="Your last name"
-              required
-            />
-          </div>
-        </div>
+			<form on:submit|preventDefault={handleSubmit} class="space-y-6">
+				<div class="grid grid-cols-1 gap-6 md:grid-cols-2">
+					<div>
+						<label for="name" class="block text-sm font-medium text-gray-700">Name</label>
+						<input
+							type="text"
+							id="name"
+							name="name"
+							bind:value={newUser.name}
+							class="mt-1 block w-full rounded-lg border border-gray-300 px-4 py-3 shadow-sm
+                     focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+							placeholder="Your name"
+							required
+						/>
+					</div>
+					<div>
+						<label for="surename" class="block text-sm font-medium text-gray-700">Last name</label>
+						<input
+							type="text"
+							id="surename"
+							name="surename"
+							bind:value={newUser.surname}
+							class="mt-1 block w-full rounded-lg border border-gray-300 px-4 py-3 shadow-sm
+                     focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+							placeholder="Your last name"
+							required
+						/>
+					</div>
+				</div>
 
-        <div>
-          <label for="username" class="block text-sm font-medium text-gray-700">Username</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            bind:value={newUser.username}
-            class="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-lg shadow-sm
-                   focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            placeholder="einstein9000"
-            required
-          />
-        </div>
+				<div>
+					<label for="username" class="block text-sm font-medium text-gray-700">Username</label>
+					<input
+						type="text"
+						id="username"
+						name="username"
+						bind:value={newUser.username}
+						class="mt-1 block w-full rounded-lg border border-gray-300 px-4 py-3 shadow-sm
+                   focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+						placeholder="einstein9000"
+						required
+					/>
+				</div>
 
-        <div>
-          <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            bind:value={newUser.email}
-            class="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-lg shadow-sm
-                   focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            placeholder="your.email@example.com"
-            required
-          />
-        </div>
+				<div>
+					<label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+					<input
+						type="email"
+						id="email"
+						name="email"
+						bind:value={newUser.email}
+						class="mt-1 block w-full rounded-lg border border-gray-300 px-4 py-3 shadow-sm
+                   focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+						placeholder="your.email@example.com"
+						required
+					/>
+				</div>
 
-        <div>
-          <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            bind:value={newUser.password}
-            class="mt-1 block w-full px-4 py-3 border border-gray-300 rounded-lg shadow-sm
-                   focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            placeholder="Minimum 8 characters"
-            required
-          />
-        </div>
+				<div>
+					<label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+					<input
+						type="password"
+						id="password"
+						name="password"
+						bind:value={newUser.password}
+						class="mt-1 block w-full rounded-lg border border-gray-300 px-4 py-3 shadow-sm
+                   focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+						placeholder="Minimum 8 characters"
+						required
+					/>
+				</div>
 
-        <div>
-          <button
-            type="submit"
-            class="w-full flex justify-center py-3 px-4 border border-transparent rounded-lg
-                   shadow-sm text-lg font-medium text-white bg-teal-950 hover:bg-cyan-800
-                   focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Create an Account
-          </button>
-        </div>
-      </form>
+				<div>
+					<button
+						type="submit"
+						class="flex w-full justify-center rounded-lg border border-transparent bg-teal-950 px-4
+                   py-3 text-lg font-medium text-white shadow-sm hover:bg-cyan-800
+                   focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+					>
+						Create an Account
+					</button>
+				</div>
+			</form>
 
-      <p class="mt-8 text-left text-sm text-gray-600">
-        Do you already have an account?
-        <a href="/login" class="font-medium text-indigo-600 hover:text-indigo-500">
-          Log in.
-        </a>
-      </p>
-    </div>
-  </div>
+			<p class="mt-8 text-left text-sm text-gray-600">
+				Do you already have an account?
+				<a href="/login" class="font-medium text-indigo-600 hover:text-indigo-500"> Log in. </a>
+			</p>
+		</div>
+	</div>
 </div>

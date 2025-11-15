3674a2adbd19db7c617306222b68481e65384cc9
Jakub Drozd <jdrozd2003@gmail.com>
2025-11-15 18:54:06 +0100

    added handling JWT tokens

--- frontend/src/lib/api/tokenApi.ts
+++ frontend/src/lib/api/tokenApi.ts
@@ -4,18 +4,69 @@
  */
 
 import type { TokenPair } from "$lib/models/tokenModels";
+import Cookies from 'js-cookie';
 
 const API_ROUTE = "http://localhost:8000/api/auth";
+const ACCESS_TOKEN_NAME = 'access_token';
+const REFRESH_TOKEN_NAME = 'refresh_token';
+
+/**
+ * Saves both tokens in cookies.
+ *
+ * @param {TokenPair} tokens
+ */
+export function setTokens(tokens: TokenPair): void {
+  // Access token - 1/288 day = 5 min
+  Cookies.set(ACCESS_TOKEN_NAME, tokens.access_token, {
+    expires: 1 / 288,
+    secure: false, // Should be `true` in prod (HTTPS required)
+    sameSite: 'Lax'
+  });
+
+  // Refresh token
+  Cookies.set(REFRESH_TOKEN_NAME, tokens.refresh_token, {
+    expires: 7,
+    secure: false, // Should be `true` in prod
+    sameSite: 'Lax'
+  });
+}
+
+/**
+ * Get JWT tokens from cookies
+ *
+ * @returns {TokenPair | null} Resolves with a new TokenPair if successful, or null otherwise
+ */
+export function getTokens(): TokenPair | null {
+  const accessToken = Cookies.get(ACCESS_TOKEN_NAME);
+  const refreshToken = Cookies.get(REFRESH_TOKEN_NAME);
+
+  if (accessToken && refreshToken) {
+    return {
+      access_token: accessToken,
+      refresh_token: refreshToken
+    };
+  }
+  return null;
+}
+
+export function logout(): void {
+  Cookies.remove(ACCESS_TOKEN_NAME);
+  Cookies.remove(REFRESH_TOKEN_NAME);
+}
 
 /**
  * Refreshes the given authentication tokens.
  *
  * Sends a POST request to the `/refresh` endpoint with the provided tokens.
  *
- * @param {TokenPair} tokens - The current access and refresh tokens.
  * @returns {Promise<TokenPair | Error>} Resolves with a new TokenPair if successful, or an Error otherwise.
  */
-export async function refresh(tokens: TokenPair): Promise<TokenPair | Error> {
+export async function refresh(): Promise<TokenPair | Error> {
+  const tokens = getTokens();
+  if (!tokens) {
+    return new Error("Brak tokenów do odświeżenia.");
+  }
+
   try {
     const response = await fetch(`${API_ROUTE}/refresh`, {
       method: 'POST',
@@ -26,14 +77,15 @@ export async function refresh(tokens: TokenPair): Promise<TokenPair | Error> {
     });
 
     if (response.ok) {
-      const tokens: TokenPair = await response.json();
-      return tokens;
+      const newTokens: TokenPair = await response.json();
+      setTokens(newTokens);
+      return newTokens;
     }
-    
+
     const error: Error = new Error(await response.text());
     return error;
   } catch (e) {
-    return new Error("Could not connect to the api.");
+    return new Error("Nie można połączyć się z API.");
   }
 }
 
@@ -42,11 +94,15 @@ export async function refresh(tokens: TokenPair): Promise<TokenPair | Error> {
  *
  * Sends a GET request to the `/validate` endpoint with the access token in the Authorization header.
  *
- * @param {TokenPair} tokens - The current access and refresh tokens.
  * @returns {Promise<boolean | Error>} Resolves with `true` if the token is valid, `false` if not, or an Error if the request fails.
  */
-export async function validate(tokens: TokenPair): Promise<boolean | Error> {
-    try {
+export async function validate(): Promise<boolean | Error> {
+  const tokens = getTokens();
+  if (!tokens) {
+    return false;
+  }
+
+  try {
     const response = await fetch(`${API_ROUTE}/validate`, {
       method: 'GET',
       headers: {
@@ -54,7 +110,7 @@ export async function validate(tokens: TokenPair): Promise<boolean | Error> {
         'Authorization': `Bearer ${tokens.access_token}`
       },
     });
-    
+
     return response.ok;
   } catch (e) {
     return new Error("Could not connect to the api.");
